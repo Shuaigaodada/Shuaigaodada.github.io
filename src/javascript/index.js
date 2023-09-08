6f58334812e8009279d021a0d3150eee050b71e4
laogao <2983536011@qq.com>
2023-09-08 02:43:43 +0000

    update

---
+++ src/javascript/index.js
@@ -0,0 +1,16 @@
+function initwebsite() {
+    setIndexContent();
+}
+function setIndexContent() {
+    setText("./src/file/FHS_content.txt", "FHS-content");
+}
+function setText(path, id) {
+    fetch(path)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(id).innerText = data;
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+window.onload = initwebsite;
