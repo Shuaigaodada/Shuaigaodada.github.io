797ed22761fa00e755b2aec0535e08d2fda0ce51
Laogao <2983536011@qq.com>
2023-11-28 18:55:16 -0600

    update

--- other/cargame/src/script/menu.js
+++ other/cargame/src/script/menu.js
@@ -1,4 +1,8 @@
 function drawMenu() {
+    // clear child node
+    while(document.body.hasChildNodes())
+        document.body.removeChild(document.body.firstChild)
+
     // load background
     document.body.style.background = 'url("' + Asset.UI.background + '")'
 
@@ -9,7 +13,7 @@ function drawMenu() {
     var startButton = document.createElement("button")
     startButton.classList.add("ui-button")
     startButton.innerHTML = "START"
-    startButton.onclick = () => {start(PvE_logic)}
+    startButton.onclick = () => { chooseMenu() }
 
     // draw UI
     document.body.appendChild(gameMenuLabel)
@@ -17,5 +21,48 @@ function drawMenu() {
 }
 
 function chooseMenu() {
+    // clear child node
+    while(document.body.hasChildNodes())
+        document.body.removeChild(document.body.firstChild)
 
+    // 创建容器
+    var container = document.createElement('div');
+    container.className = 'container';
+    document.body.appendChild(container);
+
+    // 创建并显示默认赛车图片
+    var car1Img = document.createElement('img');
+    car1Img.src = Asset.Game.car1;
+    car1Img.className = 'car-image';
+    container.appendChild(car1Img);
+
+    var car2Img = document.createElement('img');
+    car2Img.src = Asset.Game.car2;
+    car2Img.className = 'car-image';
+    container.appendChild(car2Img);
+
+    // 创建输入框和按钮
+    var uploadInput = document.createElement('input');
+    uploadInput.type = 'file';
+    uploadInput.accept = 'image/*';
+    uploadInput.className = 'input-field';
+    container.appendChild(uploadInput);
+
+    var urlInput = document.createElement('input');
+    urlInput.type = 'text';
+    urlInput.placeholder = '输入网络图片URL';
+    urlInput.className = 'input-field';
+    container.appendChild(urlInput);
+
+    var pveButton = document.createElement('button');
+    pveButton.innerHTML = '玩家 vs AI';
+    pveButton.className = 'action-button';
+    pveButton.onclick = () => { start(PvE_logic); }
+    container.appendChild(pveButton);
+
+    var pvpButton = document.createElement('button');
+    pvpButton.innerHTML = '玩家 vs 玩家';
+    pvpButton.className = 'action-button';
+    pvpButton.onclick = () => { start(PvP_logic); }
+    container.appendChild(pvpButton);
 }
