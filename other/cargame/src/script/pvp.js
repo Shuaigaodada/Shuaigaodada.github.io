1e969a2d00be41ab0bba6cee7ccdca71ea3ea9e7
Shuaigaodada <95506889+Shuaigaodada@users.noreply.github.com>
2023-12-05 13:36:40 -0600

    Update pvp.js

--- other/cargame/src/script/pvp.js
+++ other/cargame/src/script/pvp.js
@@ -1,17 +1,25 @@
 async function PvP_logic(event) {
     if(!game.started || game.isOver) return;
     if(event.key === "d" || event.key == "D") {
-        if(game.player1.x <= 80) 
+        if(game.player1.x <= 80) {
             game.player1.move(game.player1.x + game.player1.speed, game.player1.y)
-        else {
+            if(game.player1.x > 80) {
+                game.over("player1")
+                document.removeEventListener("keyup", curlogic)
+            }
+        } else {
             game.over("player1")
             document.removeEventListener("keyup", curlogic)
         }
     }
     if(event.key == "ArrowRight") {
-        if(game.player2.x <= 80)
+        if(game.player2.x <= 80) {
             game.player2.move(game.player2.x + game.player2.speed, game.player2.y)
-        else {
+            if(game.player2.x > 80) {
+                game.over("player2")
+                document.removeEventListener("keyup", curlogic)
+            }
+        } else {
             game.over("player2")
             document.removeEventListener("keyup", curlogic)
         }
