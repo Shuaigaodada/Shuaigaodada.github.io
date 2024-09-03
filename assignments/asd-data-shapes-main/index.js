4760418352da2cc360e78aa4dde1b0d6b9f1b9a0
Shuaigaodada <95506889+Shuaigaodada@users.noreply.github.com>
2024-09-03 13:04:30 +0000

    save dataShape

--- assignments/asd-data-shapes-main/index.js
+++ assignments/asd-data-shapes-main/index.js
@@ -38,19 +38,40 @@ $(document).ready(function () {
   /////////////////////////////////////////////////
 
   // TODO 1: create a new shape object and add it to the array
-  
-
+  const shape = {
+    color: "blue",
+    shape: "circle",
+    repeat: 3
+  };
+  dataShapes.push(shape);
   // TODO 2: add a new property to all data shapes
-  
+  for(let currentShape of dataShapes) {
+    if(currentShape.color === "red") {
+      currentShape.goodBehavior = "bounce";
+    } else if(currentShape.color === "blue") {
+      currentShape.goodBehavior = "blink";
+    } else {
+      currentShape.goodBehavior = "spin";
+    }
+  }
 
   // TODO 3-a: add a function that handles the static display type
-  
+  function handleStatic(data) {
+    setBackgroundWithObject(data);
+    animationDetails.displayType = 1;
+  }
 
   // TODO 4-a: add a function that handles the good display type
-  
+  function handleGood(color, shape, repeat) {
+    setBackgroundWithSimple(color, shape, repeat);
+    animationDetails.displayType = 2;
+  }
 
   // TODO 5-a: add a function that handles the bad display type
-  
+  function handleBad(data, repeat) {
+    setBackgroundWithMixed(data, repeat + 1);
+    animationDetails.displayType = 3;
+  }  
 
   /////////////////////////////////////////////////
   // BUTTON HANDLERS BELOW HERE (3-b, 4-b, 5-b) ///
@@ -58,17 +79,19 @@ $(document).ready(function () {
 
   function staticDisplay() {
     // TODO 3-b: call your handleStatic function
-    
+    handleStatic(dataShapes[currentIndex]);
   }
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    
+    let currentShape = dataShapes[currentIndex];
+    handleGood(currentShape.color, currentShape.shape, currentShape.repeat);
   }
 
   function badDisplay() {
     // TODO 5-b: call your handleBad function
-    
+    let currentShape = dataShapes[currentIndex];
+    handleBad(currentShape, currentShape.repeat);
   }
 
   /////////////////////////////////////////////////
