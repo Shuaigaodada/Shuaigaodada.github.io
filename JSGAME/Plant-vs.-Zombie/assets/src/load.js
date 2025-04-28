aa99ce9aa6a73892be155768fa1f8c2b740e9874
Shuaigaodada <95506889+Shuaigaodada@users.noreply.github.com>
2025-04-28 17:18:41 +0000

    rename

--- JSGAME/Plant-vs.-Zombie/assets/src/load.js
+++ JSGAME/Plant-vs.-Zombie/assets/src/load.js
@@ -1,6 +1,6 @@
 async function load_page() {
     Resources.add("bg1", "assets/images/Background/background1.jpg");
-
+    // Resources.load_all("assets")
     await Resources.loadAll()
     
     bg = new GameObject("bg1", new Vector2(0, 0), engine.width, engine.height);
